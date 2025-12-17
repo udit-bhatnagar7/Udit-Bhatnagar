74be7fa8faea6002548b7c19a50f607e3a60fdff
Gurpreet Singh <gurpreet.adaptiscript@gmail.com>
2025-12-17 20:54:31 +0530

    update code

---
+++ .vite/deps/react.js
@@ -0,0 +1,3 @@
+import { t as require_react } from "./react-DqDII1As.js";
+
+export default require_react();
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+
+// section imports
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Skills from '../components/Skills';
+import Services from '../components/Services';
+import Whatiwok from '../components/Whatiwok';
+import CaseStudy from '../components/CaseStudy';
+import Mainprojects from '../components/Mainprojects';
+import Contact from '../components/Contact';
+import CreativeWork from '../components/CreativeWork';
+import Experience from '../components/Experience';
+import Process from '../components/Process';
+import SEOHighlights from '../components/SEOHighlights';
+import Testimonials from '../components/Testimonials';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <main>
+        <Hero />
+        <Services/>
+        <Skills/>
+        <Whatiwok/>
+        
+        <CaseStudy/>
+        <Mainprojects/>
+        
+        <SEOHighlights/>
+        <Experience/>
+        <About /> 
+
+        <CreativeWork/>
+        <Process/>
+        <Testimonials/>
+
+        <Contact/>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
